d6d0e004933beee0682603abcd8c004b7617a904
Jagat Vasveliya <jk.vasveliya.212@gmail.com>
2022-01-12 12:29:00 +0530

    additional code

--- script.js
+++ script.js
@@ -1,51 +1,73 @@
+let firstValue = "";
+let secondValue = "";
+let operation = "";
+let answer = "";
 document.querySelectorAll("button").forEach((btn) => {
-	/*  var firstValue;
-     var secondValue;
-     var answer; */
 	btn.addEventListener("click", () => {
-		var display = document.getElementById("dis");
-		var btnValue = btn.value;
+		let display = document.getElementById("dis");
+		let btnValue = btn.value;
 
-          // advance practice code
+		// advance practice code
 
-		/* switch (btnValue) {
-			case "+":
-				if (typeof firstValue != 'undefined') {
-					answer = parseInt(firstValue) + parseInt(display.value);
-					firstValue = answer;
-                         display.value = '';
-				} else {
-					firstValue = display.value;
-					display.value = "";
-				}
-				break;
+		switch (btnValue) {
 			case "=":
-				alert(firstValue);
-				display.value = answer;
+				answer = firstValue + operation + display.value;
+				display.value = eval(answer.replace("%", "/100*"));
+				firstValue = "";
+				secondValue = "";
+				operation = "";
+				break;
+
+			case "C":
+				display.value = "";
+				firstValue = "";
+				secondValue = "";
+				break;
+
+			case "erase":
+				display.value = display.value.slice(0, -1);
 				break;
 
 			default:
-				display.value = display.value + btnValue;
+				if (
+					btn.value == "+" ||
+					btn.value == "-" ||
+					btn.value == "*" ||
+					btn.value == "/" ||
+					btn.value == "%"
+				) {
+					if (firstValue != "") {
+						answer = firstValue + operation + display.value;
+						firstValue = eval(answer.replace("%", "/100*"));
+						display.value = "";
+						operation = btn.value;
+					} else {
+						firstValue = display.value;
+						operation = btn.value;
+						display.value = "";
+					}
+				} else {
+					display.value = display.value + btnValue;
+				}
 				break;
-		} */
+		}
 
-		switch (btnValue) {
+		/* switch (btnValue) {
 			case "C":
 				display.value = "";
 				break;
 
 			case "erase":
-				display.value = (display.value).slice(0, -1);
+				display.value = display.value.slice(0, -1);
 				break;
 
 			case "=":
-				display.value = eval((display.value).replace('%', '/100*'));
+				display.value = eval(display.value.replace("%", "/100*"));
 				break;
-
 
 			default:
 				display.value = display.value + btn.value;
 				break;
-		}
+		} */
 	});
 });
